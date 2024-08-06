71794153a82db5e46fa67c63f191a0e47879d33b
longnt <nice231096@gmail.com>
2024-08-06 17:59:53 +0700

    setup Catalog service with CQRS

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,39 @@
+﻿namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(
+    string Name, 
+    List<string> Category, 
+    string Description, 
+    string ImageFile, 
+    decimal Price
+) : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler 
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    private readonly IDocumentSession _documentSession;
+
+    public CreateProductCommandHandler(IDocumentSession documentSession)
+    {
+        _documentSession = documentSession;
+    }
+
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price,
+        };
+
+        _documentSession.Store(product);
+        await _documentSession.SaveChangesAsync(cancellationToken);
+
+        return new CreateProductResult(product.Id);
+    }
+}
