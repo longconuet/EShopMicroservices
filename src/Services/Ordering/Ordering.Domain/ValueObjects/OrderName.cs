caf6ae65ecd9103b2f1fa6c29662fecd3663e679
longnt <nice231096@gmail.com>
2024-08-20 16:15:42 +0700

    domain event

---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
@@ -0,0 +1,16 @@
+﻿namespace Ordering.Domain.ValueObjects;
+
+public record OrderName
+{
+    private const int DetfaultLength = 5;
+    public string Value {  get; }
+    private OrderName(string value) => Value = value;
+
+    public static OrderName Of(string value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(value);
+        ArgumentOutOfRangeException.ThrowIfNotEqual(value.Length, DetfaultLength);
+
+        return new OrderName(value);
+    }
+}
