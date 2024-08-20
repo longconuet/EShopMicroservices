d36dffe4c30a38986bef0ff16a30b3347d8e791d
longnt <nice231096@gmail.com>
2024-08-20 11:56:44 +0700

    ordering DDD

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Aggregate.cs
@@ -0,0 +1,20 @@
+﻿namespace Ordering.Domain.Abstractions;
+
+public abstract class Aggregate<TId> : Entity<TId>, IAggregate<TId>
+{
+    private readonly List<IDomainEvent> _domainEvent = new();
+
+    public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvent.AsReadOnly();
+
+    public void AddDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvent.Add(domainEvent);
+    }
+
+    public IDomainEvent[] ClearDomainEvents()
+    {
+        IDomainEvent[] dequeuedEvents = _domainEvent.ToArray();
+        _domainEvent.Clear();
+        return dequeuedEvents;
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Database");
+
+        //services.AddDbContext<ApplicationDbContext>(options =>
+        //{
+        //    options.UseSqlServer(connectionString);
+        //});
+
+        return services;
+    }
+}
