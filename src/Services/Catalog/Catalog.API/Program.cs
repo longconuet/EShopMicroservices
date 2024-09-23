9302a76244b927168f3920fbfd0b0864cd89f003
LONGNT-DF\longn <longnt@reactplus.com>
2024-09-23 18:30:12 +0700

    config serilog

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,7 +1,10 @@
 using BuildingBlocks.Exceptions.Handler;
 using Catalog.API.Data;
+using Elastic.Serilog.Sinks;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,6 +31,24 @@ builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 builder.Services.AddHealthChecks()
     .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
+
+// Configure Serilog
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .ReadFrom.Configuration(builder.Configuration)
+    .WriteTo.Console() // Optional: Log to console
+    .WriteTo.Elasticsearch(new Serilog.Sinks.Elasticsearch.ElasticsearchSinkOptions(new Uri(builder.Configuration["ElasticConfiguration:Uri"]!))
+    {
+        AutoRegisterTemplate = true,            // Automatically register the ECS template
+        IndexFormat = "ecs-logs-{0:yyyy.MM}",   // Index name format (you can change this)
+        EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog,
+        NumberOfShards = 2,
+        NumberOfReplicas = 1
+    })
+    .CreateLogger();
+
+// Replace default logger
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 
