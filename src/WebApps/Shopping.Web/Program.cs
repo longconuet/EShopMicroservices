0dca67a66bacf93dccfbd607e9193d5ae8548908
LONGNT-DF\longn <longnt@reactplus.com>
2024-09-26 18:22:18 +0700

    edit httpclient config

--- src/WebApps/Shopping.Web/Program.cs
+++ src/WebApps/Shopping.Web/Program.cs
@@ -1,14 +1,23 @@
+using Common.Logging;
 using Shopping.Web.Services;
 using Shopping.Web.Services.IService;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddHttpClient();
-builder.Services.AddHttpClient<ICatalogService, CatalogService>();
+builder.Services.AddTransient<LoggingDelegatingHandler>();
+
+
+builder.Services.AddHttpClient<ICatalogService, CatalogService>(c =>
+    {
+        c.BaseAddress = new Uri("https://localhost:5050"); // builder.Configuration.GetValue<string>("CatalogAPI")!)
+        c.DefaultRequestHeaders.Add("Accept", "application/json");
+    })
+    .AddHttpMessageHandler<LoggingDelegatingHandler>();
 
 builder.Services.AddScoped<IBaseService, BaseService>();
 builder.Services.AddScoped<ICatalogService, CatalogService>();
+
 
 builder.Services.AddControllersWithViews();
 
--- src/WebApps/Shopping.Web/Services/BaseService.cs
+++ src/WebApps/Shopping.Web/Services/BaseService.cs
@@ -7,49 +7,46 @@ namespace Shopping.Web.Services;
 public class BaseService : IBaseService
 {
     private readonly IHttpClientFactory _httpClientFactory;
-    private readonly string _clientName = "eshop-api";
+    protected readonly HttpClient _httpClient;
 
     public BaseService(IHttpClientFactory httpClientFactory)
     {
         _httpClientFactory = httpClientFactory;
+        _httpClient = _httpClientFactory.CreateClient("eshop-api");
     }
 
     public async Task<T> GetAsync<T>(string url)
     {
-        var client = _httpClientFactory.CreateClient(_clientName);
-        var response = await client.GetAsync(url);
+        var response = await _httpClient.GetAsync(url);
 
         response.EnsureSuccessStatusCode(); // Throws exception if status code is not 2xx
 
         var responseData = await response.Content.ReadAsStringAsync();
-        return JsonConvert.DeserializeObject<T>(responseData);
+        return JsonConvert.DeserializeObject<T>(responseData)!;
     }
 
     public async Task<TResponse> PostAsync<TRequest, TResponse>(string url, TRequest requestData)
     {
-        var client = _httpClientFactory.CreateClient(_clientName);
         var content = new StringContent(JsonConvert.SerializeObject(requestData), Encoding.UTF8, "application/json");
-        var response = await client.PostAsync(url, content);
+        var response = await _httpClient.PostAsync(url, content);
 
         response.EnsureSuccessStatusCode();
 
         var responseData = await response.Content.ReadAsStringAsync();
-        return JsonConvert.DeserializeObject<TResponse>(responseData);
+        return JsonConvert.DeserializeObject<TResponse>(responseData)!;
     }
 
     public async Task PutAsync<TRequest>(string url, TRequest requestData)
     {
-        var client = _httpClientFactory.CreateClient(_clientName);
         var content = new StringContent(JsonConvert.SerializeObject(requestData), Encoding.UTF8, "application/json");
-        var response = await client.PutAsync(url, content);
+        var response = await _httpClient.PutAsync(url, content);
 
         response.EnsureSuccessStatusCode();
     }
 
     public async Task DeleteAsync(string url)
     {
-        var client = _httpClientFactory.CreateClient(_clientName);
-        var response = await client.DeleteAsync(url);
+        var response = await _httpClient.DeleteAsync(url);
 
         response.EnsureSuccessStatusCode();
     }
