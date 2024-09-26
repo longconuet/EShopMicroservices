5d8ceb7769521a2b930f68029f10e0d9a8bfcef3
LONGNT-DF\longn <longnt@reactplus.com>
2024-09-26 13:37:27 +0700

    config serilog sink to elasticsearch

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,10 +1,16 @@
 using BuildingBlocks.Exceptions.Handler;
 using Catalog.API.Data;
+using Elastic.Channels;
+using Elastic.CommonSchema.Serilog;
+using Elastic.Ingest.Elasticsearch.DataStreams;
+using Elastic.Ingest.Elasticsearch;
 using Elastic.Serilog.Sinks;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Configuration;
 using Serilog;
 using Serilog.Sinks.Elasticsearch;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -33,22 +39,61 @@ builder.Services.AddHealthChecks()
     .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
 
 // Configure Serilog
-Log.Logger = new LoggerConfiguration()
-    .Enrich.FromLogContext()
-    .ReadFrom.Configuration(builder.Configuration)
-    .WriteTo.Console() // Optional: Log to console
-    .WriteTo.Elasticsearch(new Serilog.Sinks.Elasticsearch.ElasticsearchSinkOptions(new Uri(builder.Configuration["ElasticConfiguration:Uri"]!))
-    {
-        AutoRegisterTemplate = true,            // Automatically register the ECS template
-        IndexFormat = "ecs-logs-{0:yyyy.MM}",   // Index name format (you can change this)
-        EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog,
-        NumberOfShards = 2,
-        NumberOfReplicas = 1
-    })
-    .CreateLogger();
+//Log.Logger = new LoggerConfiguration()
+//    .Enrich.FromLogContext()
+//    .ReadFrom.Configuration(builder.Configuration)
+//    .WriteTo.Console() // Optional: Log to console
+//    .WriteTo.Elasticsearch(new Serilog.Sinks.Elasticsearch.ElasticsearchSinkOptions(new Uri(builder.Configuration["ElasticConfiguration:Uri"]!))
+//    {
+//        AutoRegisterTemplate = true,            // Automatically register the ECS template
+//        IndexFormat = "ecs-logs-{0:yyyy.MM}",   // Index name format (you can change this)
+//        EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog,
+//        NumberOfShards = 2,
+//        NumberOfReplicas = 1
+//    })
+//    .CreateLogger();
+
+//var localHostElasticURL = builder.Configuration["ElasticConfiguration:Uri"]!;
+//string indexFormat = "stackOverFlowTestindex";
+//Log.Logger = new LoggerConfiguration()
+//    .Enrich.FromLogContext()
+//    .WriteTo.Elasticsearch(new[] { new Uri(localHostElasticURL) },
+//    options =>
+//    {
+//        options.DataStream = new DataStreamName(indexFormat);
+//        options.TextFormatting = new EcsTextFormatterConfiguration();
+//        options.BootstrapMethod = BootstrapMethod.Failure;
+//        options.ConfigureChannel = channelOptions =>
+//        {
+//            channelOptions.BufferOptions = new BufferOptions();
+//        };
+//    },
+//    configureTransport =>
+//    {
+//        configureTransport.ServerCertificateValidationCallback((_, _, _, _) => true);
+//    })
+//    .ReadFrom.Configuration(builder.Configuration)
+//    .CreateLogger();
 
 // Replace default logger
-builder.Host.UseSerilog();
+builder.Host.UseSerilog((context, configuration) =>
+{
+    configuration
+        .Enrich.FromLogContext()
+        .Enrich.WithMachineName()
+        .WriteTo.Console()
+        .WriteTo.Elasticsearch(new Serilog.Sinks.Elasticsearch.ElasticsearchSinkOptions(new Uri(builder.Configuration["ElasticConfiguration:Uri"]!))
+        {
+            AutoRegisterTemplate = true,            // Automatically register the ECS template
+            //IndexFormat = "ecs-logs-{0:yyyy.MM}",   // Index name format (you can change this)
+            IndexFormat = $"applogs-{Assembly.GetExecutingAssembly().GetName().Name!.ToLower().Replace(".", "-")}-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-logs-{DateTime.UtcNow:yyyy-MM}",   // Index name format (you can change this)
+            EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog,
+            NumberOfShards = 2,
+            NumberOfReplicas = 1
+        })
+        .Enrich.WithProperty("Enviroment", context.HostingEnvironment.EnvironmentName)
+        .ReadFrom.Configuration(builder.Configuration);
+});
 
 var app = builder.Build();
 
