ad20393a4765411aff4b39e8bcfa0fc7057b0653
LONGNT-DF\longn <longnt@reactplus.com>
2024-10-03 14:14:04 +0700

    add Web Status prj using watch dog

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,6 +1,8 @@
 using Common.Logging;
 using Discount.Grpc.Data;
 using Discount.Grpc.Services;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 
@@ -14,11 +16,22 @@ builder.Services.AddDbContext<DiscountDbContext>(options =>
 
 builder.Host.UseSerilog(SeriLogger.Configure);
 
+builder.Services.AddGrpcHealthChecks();
+    //.AddSqlite(builder.Configuration.GetConnectionString("Database")!);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseMigration();
 app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
+
+app.MapGrpcHealthChecksService();
 
 app.Run();
